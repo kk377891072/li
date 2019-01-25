911a9f6dc8b1f57846761d16c4b46f60b6f0ac49
lenovo <lenovo@LAPTOP-CV8SFCL6>
2019-01-25 10:50:07 +0800

    该

--- LingJia_web/src/com/nnn/dzr/dao/BaseDao.java
+++ LingJia_web/src/com/nnn/dzr/dao/BaseDao.java
@@ -27,6 +27,10 @@ public class BaseDao {
 		}
 		return conn;
 	}
+	
+	public static void main(String[] args) {
+		System.out.println("¶¥¶¥¶¥");
+	}
 
 	// ÔöÉ¾¸Ä(update,insert,delete)
 	public String executeUpdate(String sql, Object... objects) {
